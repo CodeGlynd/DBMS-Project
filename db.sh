46075a033b37b666510606982a4d18d38ba53ae8
unknown <ali405930@fci.bu.edu.eg>
2024-08-08 19:17:24 +0300

    refactor: clean code

--- db.sh
+++ db.sh
@@ -1,31 +1,6 @@
 #!/bin/bash
 
-#---Input Name validation-------------------------------------
-
-function ValInputName {
-
-	if [ -z "$1" ]
-	then
-		echo "Name field can't be empty"
-		return 1
-
-	elif [[ $1 == *" "* ]]
-	then
-		echo "Name shouldn't include spaces"
-		return 1
-
-	elif [[ $1 =~ [^a-zA-Z0-9_] ]]
-	then
-		echo "Name shouldn't have special characters"
-		return 1
-
-	elif [[ "$1" =~ ^[0-9] ]]
-	then
-		echo "Name shouldn't begin with a number"
-		return 1
-	
-	fi
-}
+source ./validationfuncs.sh
 
 
 #-------------------------------Main Menu Functions----------------------------------
@@ -113,6 +88,7 @@ function DropDB {
 	while true 
 	do
 		read -p "Enter DataBase name: " DBName
+
 
 		ValInputName $DBName
 		if [ $? -eq 0 ]
